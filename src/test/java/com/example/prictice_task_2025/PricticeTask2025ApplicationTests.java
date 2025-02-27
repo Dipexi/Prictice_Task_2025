93b350837ff265ad76ff469e769403d74f986205
Евгений Ковалев <kasford222@gmail.com>
2025-02-28 00:37:54 +0300

    Initial commit

---
+++ src/test/java/com/example/prictice_task_2025/PricticeTask2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prictice_task_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricticeTask2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
